bcdbede8faee02dd0f9281a4ab0ffa66100d1c0c
Jacob Viktor Winther <jacobw566@gmail.com>
2025-05-04 12:17:34 +0200

    updated

--- AGM.py
+++ AGM.py
@@ -5,33 +5,39 @@ from Proposition import Proposition
 import sympy as sp
 
 def success(Belief, phi):
-    newBase = Belief.expand(phi)
+    newBase = Belief.expand(phi.premise)
     return phi in newBase.beliefs
 
 def inclusion(Belief, phi):
-    newBase = contract_belief_base(Belief, phi)
+    belief_base_with_priorities = [(belief, idx + 1) for idx, belief in enumerate(Belief.beliefs)]
+    newBase = contract_belief_base(belief_base_with_priorities, phi)
     return set(newBase).issubset(set(Belief.beliefs))
 
 def vacuity(Belief, phi):
-    if not resolution(Belief, phi):
-        newBase = contract_belief_base(Belief, phi)
+    belief_base_with_priorities = [(belief, idx + 1) for idx, belief in enumerate(Belief.beliefs)]
+    combined_beliefs = Proposition(" and ".join([belief.premise for belief in Belief.beliefs]))
+    if not resolution(combined_beliefs, phi):
+        newBase = contract_belief_base(belief_base_with_priorities, phi)
         return newBase == Belief.beliefs
     return True
 
 def consistency(Belief, phi):
-    newBase = contract_belief_base(Belief, phi)
+    belief_base_with_priorities = [(belief, idx + 1) for idx, belief in enumerate(Belief.beliefs)]
+    newBase = contract_belief_base(belief_base_with_priorities, phi)
     return consistent(newBase)
 
 def extensionality(Belief, phi1, phi2):
+    belief_base_with_priorities = [(belief, idx + 1) for idx, belief in enumerate(Belief.beliefs)]
     if equivalent(phi1, phi2):
-        return contract_belief_base(Belief, phi1) == contract_belief_base(Belief, phi2)
+        return contract_belief_base(belief_base_with_priorities, phi1) == contract_belief_base(belief_base_with_priorities, phi2)
     return True
 
 def consistent(Belief):
-    Belief.symbolic_form()
-    local_sym = {str(sym): sym for sym in Belief.symbolic}
+    combined_beliefs = Proposition(" and ".join([belief.premise for belief, _ in Belief]))
+    combined_beliefs.symbolic_form()
+    local_sym = {str(sym): sym for sym in combined_beliefs.symbolic}
     local_sym.update({'Eq': sp.Equivalent})
-    expr = sp.parsing.sympy_parser.parse_expr(Belief.expression, local_dict = local_sym, evaluate = False)
+    expr = sp.parsing.sympy_parser.parse_expr(combined_beliefs.expression, local_dict = local_sym, evaluate = False)
     return bool(sp.satisfiable(expr))
 
 def equivalent(phi1, phi2):
